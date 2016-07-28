86101a885d8690692e9235420b66b921e926f5e0
Brendan Wu <Brendan@Brendans-MacBook-Pro.local>
2016-07-28 01:25:15 -0400

    jumping delay bug fixed

--- Assets/Scripts/Camera.cs
+++ Assets/Scripts/Camera.cs
@@ -5,14 +5,15 @@ public class Camera : MonoBehaviour {
 
 	public Transform lookAt;
 	public float smoothSpeed = 0.125f;
-
 	private bool smooth = true;
 	private Vector3 offset = new Vector3(0, 4f, -10f);
+
 
 	// Use this for initialization
 	void Start () {
 	
 	}
+
 	
 	// Update is called once per frame
 	void LateUpdate () {
